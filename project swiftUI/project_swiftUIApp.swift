60a1fe245a4f01cf039f33a918eaad43980c8262
dhairya990 <dhairyabhardwaj007@gmail.com>
2024-07-03 14:31:03 +0530

    Initial Commit

---
+++ project swiftUI/project_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  project_swiftUIApp.swift
+//  project swiftUI
+//
+//  Created by Dhairya bhardwaj on 03/07/24.
+//
+
+import SwiftUI
+
+@main
+struct project_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
